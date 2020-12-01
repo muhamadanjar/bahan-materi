8411b20b6ed2224d001fdacc293f7961b8324cd3
Muhamad Anjar <arvanria@gmail.com>
2020-12-01 23:59:59 +0700

    add radius nearest

--- app/main/views.py
+++ app/main/views.py
@@ -71,6 +71,7 @@ def new_photos():
 def get_nearest_poi():
     lat = request.args.get('lat', -6.6160933)
     lon = request.args.get('lon', 106.8266368)
+    radius = request.args.get('radius', 1000)
     lonlat = str(lon) + ' ,' + str(lat)
     engine = create_engine(config['default'].SQLALCHEMY_BINDS['kotabogor'])
     sql = """SELECT kegiatan, nama_objek, 
@@ -79,7 +80,7 @@ def get_nearest_poi():
         wkb_geometry  FROM tematik.gis_poi 
         WHERE 
             ST_DWithin(wkb_geometry, 
-                ST_TRANSFORM(ST_SetSRID(ST_MakePoint(%s), 4326), 32748), 1000) """ % (lonlat)
+                ST_TRANSFORM(ST_SetSRID(ST_MakePoint(%s), 4326), 32748), %s) """ % (lonlat, radius)
     with engine.connect() as con:
         result = con.execute(sql)
         resultkey = con.execute(sql).keys()
@@ -92,17 +93,22 @@ def get_nearest_poi():
             output.append(inner)
     return jsonify({'status':'success', 'data': output})
 
+
 @main.route('/get_bbox', methods=['GET'])
 def get_bbox():
     engine = create_engine(config['default'].SQLALCHEMY_BINDS['kotabogor'])
-    minx = request.args.get('minx', 0)
-    miny = request.args.get('miny', 0)
-    maxx = request.args.get('maxx', 0)
-    maxy = request.args.get('maxy', 0)
+    minx = request.args.get('xmin', 0)
+    miny = request.args.get('ymin', 0)
+    maxx = request.args.get('xmax', 0)
+    maxy = request.args.get('ymax', 0)
     bbox = str(minx) + ',' + str(miny) + ',' + str(maxx) + ',' + str(maxy)
     sql = """SELECT kegiatan, nama_objek  FROM tematik.gis_poi 
         WHERE ST_Intersects(
-            ST_SetSRID(ST_MakePoint(ST_X(wkb_geometry), st_y(wkb_geometry)), 4326), 
+            ST_SetSRID(
+                ST_MakePoint(
+                    ST_X(st_transform(wkb_geometry, 4326)), 
+                    ST_Y(st_transform(wkb_geometry, 4326))
+                ), 4326), 
             ST_MakeEnvelope(%s, 4326)
         )='t' """ % (bbox)
     with engine.connect() as con:
@@ -116,6 +122,7 @@ def get_bbox():
                 inner[key] = str(val)
             output.append(inner)
         return jsonify({'status':'success', 'data': output})
+
 
 @main.route('/get_administrasi')
 def get_administrasi():
@@ -133,3 +140,47 @@ def get_administrasi():
                 inner[key] = vf
             output.append(inner)
         return jsonify({'status': 'success', 'data': output})
+
+
+@main.route('/check_point_in_poly')
+def get_checkin_points_in_poly():
+    engine = create_engine(config['default'].SQLALCHEMY_BINDS['kotabogor'])
+    kode_kec = request.args.get('kode', 3271010)
+    sql = """ 
+    SELECT kegiatan , nama_objek , st_x(st_transform(wkb_geometry, 4326)) as longitude, st_y(st_transform(wkb_geometry, 4326)) as latitude
+        FROM tematik.gis_poi 
+    WHERE ST_Contains(
+        (SELECT geom from administrasi.gis_admin_kec where kode_kec = %s limit 1), 
+        tematik.gis_poi.wkb_geometry
+    );
+    """ % (kode_kec)
+    with engine.connect() as con:
+        result = con.execute(text(sql))
+        resultkey = result.keys()
+        rows = result.fetchall()
+        output = []
+        for item in rows:
+            inner = {}
+            for key, val in zip(resultkey, item):
+                vf = val if key == 'geojson' else str(val)
+                inner[key] = vf
+            output.append(inner)
+        return jsonify({'status': 'success', 'data': output, 'params': kode_kec})
+
+
+@main.route('/get_pip')
+def get_pip():
+    engine = create_engine(config['default'].SQLALCHEMY_BINDS['kotabogor'])
+    lat = request.args.get('lat', -6.6160933)
+    lon = request.args.get('lon', 106.8266368)
+    lonlat = str(lon) + ' ,' + str(lat)
+    sql = """ SELECT kecamatan, kode_kec FROM administrasi.gis_admin_kec gak
+              WHERE ST_Contains(
+	            st_transform(gak.geom, 4326), 
+	            st_setsrid(st_makepoint(%s), 4326))""" % (lonlat)
+    
+    with engine.connect() as con:
+        result = con.execute(text(sql))
+        rows = result.fetchall()
+        jml = len(rows)
+        return jsonify({'status': 'success', 'data': jml})
